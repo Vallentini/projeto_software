f506bed9fb9adbc3ac67cb7f1cd3070d260dc72f
Vallentini <vallentinigabrielli.silva@gmail.com>
2024-08-16 16:10:00 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <div class="topnav">
+      <nav>
+        <div id="links-menu">
+          <ul>
+            <li><a href="index.html"><b>HOME</b></a></li>
+            <li><a href=""><b>OFERTAS</b></a></li>
+            <li><a href=""><b>QUEM SOMOS?</b></a></li>
+            <li><a href=""><b>ATENDIMENTO</b></a></li>
+          </ul>
+        </div>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+          <i class="fa fa-bars"></i>
+        </a>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
